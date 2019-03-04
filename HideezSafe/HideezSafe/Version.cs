c453f14efe6d09cc81a63275830d8a03c1bf44a2
Illya Irachek <illyairachek@gmail.com>
2019-03-04 15:42:56 +0200

    Initial project structure

---
+++ HideezSafe/HideezSafe/Version.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Resources;
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+// Client application version
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
