6ae6651469250e6dd44f1b35e23368ef3e8c71a8
Illya Irachek <illyairachek@gmail.com>
2019-06-04 17:26:43 +0300

    Updated version

--- HideezSafe/HideezSafe/Version.cs
+++ HideezSafe/HideezSafe/Version.cs
@@ -13,6 +13,6 @@ using System.Resources;
 // [assembly: AssemblyVersion("1.0.*")]
 
 // Client application version
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyVersion("3.0.1")]
+[assembly: AssemblyFileVersion("3.0.1")]
 [assembly: NeutralResourcesLanguage("en")]
