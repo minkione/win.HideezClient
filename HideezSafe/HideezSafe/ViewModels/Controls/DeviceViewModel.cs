c58ea856b596bac332cd59564cdaa7916c3b1018
Illya Irachek <illyairachek@gmail.com>
2019-06-26 12:23:34 +0300

    DeviceViewModel properties changed to expression

--- HideezSafe/HideezSafe/ViewModels/Controls/DeviceViewModel.cs
+++ HideezSafe/HideezSafe/ViewModels/Controls/DeviceViewModel.cs
@@ -1,19 +1,11 @@
-﻿using Hideez.SDK.Communication.Interfaces;
-using Hideez.SDK.Communication.Remote;
-using HideezSafe.HideezServiceReference;
-using HideezSafe.Models;
+﻿using HideezSafe.Models;
 using HideezSafe.Modules;
 using HideezSafe.Modules.Localize;
 using HideezSafe.Modules.ServiceProxy;
 using HideezSafe.Mvvm;
-using MvvmExtensions.Commands;
 using NLog;
-using System;
 using System.Collections.ObjectModel;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Input;
 
 namespace HideezSafe.ViewModels
 {
@@ -46,50 +38,15 @@ namespace HideezSafe.ViewModels
         [Localization]
         public string TypeName { get { return device.TypeName; } }
 
-        public string Id
-        {
-            get { return device.Id; }
-        }
-
-        public string Name
-        {
-            get { return device.Name; }
-        }
-
-        public bool IsConnected
-        {
-            get { return device.IsConnected; }
-        }
-
-        public double Proximity
-        {
-            get { return device.Proximity; }
-        }
-
-        public int Battery
-        {
-            get { return device.Battery; }
-        }
-
-        public string OwnerName
-        {
-            get { return device.OwnerName; }
-        }
-
-        public string SerialNo
-        {
-            get { return device.SerialNo; }
-        }
-
-        public bool IsInitializing
-        {
-            get { return device.IsInitializing; }
-        }
-
-        public bool IsInitialized
-        {
-            get { return device.IsInitialized; }
-        }
+        public string Id => device.Id;
+        public string Name => device.Name;
+        public bool IsConnected => device.IsConnected;
+        public double Proximity => device.Proximity;
+        public int Battery => device.Battery;
+        public string OwnerName => device.OwnerName;
+        public string SerialNo => device.SerialNo;
+        public bool IsInitializing => device.IsInitializing;
+        public bool IsInitialized => device.IsInitialized;
 
         public ObservableCollection<MenuItemViewModel> MenuItems { get; }
 
