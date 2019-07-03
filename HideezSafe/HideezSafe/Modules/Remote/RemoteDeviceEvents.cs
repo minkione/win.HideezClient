78ebd971d2e934489cef29d5abfd29cd6605ed41
Illya Irachek <illyairachek@gmail.com>
2019-07-03 15:00:45 +0300

    RemoteDeviceConnection is broken down into RemoteDeviceEvents and RemoteDeviceCommands

---
+++ HideezSafe/HideezSafe/Modules/Remote/RemoteDeviceEvents.cs
@@ -0,0 +1,58 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using Hideez.SDK.Communication;
+using Hideez.SDK.Communication.Remote;
+using HideezSafe.Messages.Remote;
+using System;
+
+namespace HideezSafe.Modules.Remote
+{
+    class RemoteDeviceEvents : IRemoteEvents
+    {
+        readonly IMessenger _messenger;
+
+        public RemoteDeviceEvents(IMessenger messenger)
+        {
+            _messenger = messenger;
+
+            _messenger.Register<Remote_RssiReceivedMessage>(this, OnRssiReceivedMessage);
+            _messenger.Register<Remote_BatteryChangedMessage>(this, OnBatteryChangedMessage);
+            _messenger.Register<Remote_StorageModifiedMessage>(this, OnStorageModified);
+        }
+
+        public event EventHandler<double> RssiReceived;
+        public event EventHandler<int> BatteryChanged;
+        public event EventHandler StorageModified;
+
+        // Temporary duct tape, until IRemoteDeviceConnection is refactored
+        public RemoteDevice RemoteDevice { get; set; }
+
+        void OnRssiReceivedMessage(Remote_RssiReceivedMessage msg)
+        {
+            if (IsMessageFromCurrentDevice(msg))
+                RssiReceived?.Invoke(this, msg.Rssi);
+        }
+
+        void OnBatteryChangedMessage(Remote_BatteryChangedMessage msg)
+        {
+            if (IsMessageFromCurrentDevice(msg))
+                BatteryChanged?.Invoke(this, msg.Battery);
+        }
+
+        void OnStorageModified(Remote_StorageModifiedMessage msg)
+        {
+            if (IsMessageFromCurrentDevice(msg))
+                StorageModified?.Invoke(this, EventArgs.Empty);
+        }
+
+        bool IsMessageFromCurrentDevice(Remote_BaseMessage msg)
+        {
+            if (string.IsNullOrWhiteSpace(msg.SerialNo))
+                return false;
+
+            if (RemoteDevice == null)
+                return false;
+
+            return msg.SerialNo == RemoteDevice.SerialNo;
+        }
+    }
+}
--- HideezSafe/HideezSafe/Modules/Remote/RemoteDeviceFactory.cs
+++ HideezSafe/HideezSafe/Modules/Remote/RemoteDeviceFactory.cs
@@ -1,5 +1,6 @@
 ﻿using GalaSoft.MvvmLight.Messaging;
 using Hideez.SDK.Communication.Remote;
+using HideezSafe.Modules.Remote;
 using HideezSafe.Modules.ServiceProxy;
 using System.Threading.Tasks;
 
@@ -20,12 +21,13 @@ namespace HideezSafe.Modules
         {
             var connectionId = await _serviceProxy.GetService().EstablishRemoteDeviceConnectionAsync(serialNo, channelNo);
 
-            var remoteConnection = new RemoteDeviceConnection(_serviceProxy, _messenger);
+            var remoteCommands = new RemoteDeviceCommands(_serviceProxy);
+            var remoteEvents = new RemoteDeviceEvents(_messenger);
 
-            // Todo: Break up remoteConnection into two separate separate classes: RemoteCommands and RemoteEvents
-            var device = new RemoteDevice(connectionId, remoteConnection, remoteConnection);
+            var device = new RemoteDevice(connectionId, remoteCommands, remoteEvents);
 
-            remoteConnection.RemoteDevice = device;
+            remoteCommands.RemoteDevice = device;
+            remoteEvents.RemoteDevice = device;
 
             return device;
         }
