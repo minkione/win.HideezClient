5782f72928978772c98675f2a6c6a754b5263ac9
Igor <ICh.Hideez@gmail.com>
2019-07-05 17:13:02 +0300

    Fix verification

--- HideezService/ServiceLibrary.Implementation/HideezService_SDK.cs
+++ HideezService/ServiceLibrary.Implementation/HideezService_SDK.cs
@@ -12,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ServiceLibrary.Implementation
@@ -34,9 +35,10 @@ namespace ServiceLibrary.Implementation
             var sdkLogger = new NLogger();
 
 #if DEBUG
-            _log.Info(">>>>>> Verifying error codes:");
+            _log.Info($">>>>>> Verifying error codes.");
             var _hideezExceptionLocalization = new HideezExceptionLocalization(sdkLogger);
-            _hideezExceptionLocalization.VerifyResourcesForErrorCode(CultureInfo.CurrentCulture);
+            bool isVerified = _hideezExceptionLocalization.VerifyResourcesForErrorCode(new CultureInfo("en"));
+            _log.Info($">>>>>> Verifying error codes resalt: {isVerified}");
 #endif
 
             // Combined path evaluates to '%ProgramData%\\Hideez\\Bonds'
