43566f6bcee1e129e112515c8a492aca20ee61c5
Igor <ICh.Hideez@gmail.com>
2019-08-12 17:10:44 +0300

    Color converter

---
+++ Samples/WinSampleApp/Converter/BoolToColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WinSampleApp.Converter
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((bool)value == BoolValue) ? OneBrush : TwoBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool BoolValue { get; set; }
+
+        public Brush OneBrush { get; set; }
+
+        public Brush TwoBrush { get; set; }
+
+    }
+}
