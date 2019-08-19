711056a86ac65395df23e9aea6ef5fe40e321182
Denys Zaliznyak <dz@hideez.com>
2019-08-19 11:04:41 +0300

    v3.0.20.2

--- Version.cs
+++ Version.cs
@@ -13,6 +13,6 @@ using System.Resources;
 // [assembly: AssemblyVersion("1.0.*")]
 
 // Client application version
-[assembly: AssemblyVersion("3.0.20")]
-[assembly: AssemblyFileVersion("3.0.20")]
+[assembly: AssemblyVersion("3.0.20.2")]
+[assembly: AssemblyFileVersion("3.0.20.2")]
 [assembly: NeutralResourcesLanguage("en")]
