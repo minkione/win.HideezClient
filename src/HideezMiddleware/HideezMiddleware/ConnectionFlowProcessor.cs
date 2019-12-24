374133157a92a605f6a953ceeca9e3131f1fd68b
Illya Irachek <illyairachek@gmail.com>
2019-12-24 15:34:23 +0200

    Fixed exception message formatting for user

--- src/HideezMiddleware/HideezMiddleware/ConnectionFlowProcessor.cs
+++ src/HideezMiddleware/HideezMiddleware/ConnectionFlowProcessor.cs
@@ -225,7 +225,7 @@ namespace HideezMiddleware
             }
             catch (Exception ex)
             {
-                errorMessage = ex.Message;
+                errorMessage = HideezExceptionLocalization.GetErrorAsString(ex.Message);
             }
             finally
             {
