bbe11475d0812f83326c7bd93ee92757b11690fd
Illya Irachek <illyairachek@gmail.com>
2020-01-14 19:24:57 +0200

    Bindings for DeviceInfoControl and SelectableDeviceList

---
+++ src/HideezClient/HideezClient/Controls/SmallDeviceInfoControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using HideezClient.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HideezClient.Controls
+{
+    /// <summary>
+    /// Interaction logic for DeviceInfo.xaml
+    /// </summary>
+    public partial class SmallDeviceInfoControl : UserControl
+    {
+        public DeviceInfoViewModel Device
+        {
+            get { return (DeviceInfoViewModel)GetValue(DeviceProperty); }
+            set { SetValue(DeviceProperty, value); }
+        }
+
+        public static readonly DependencyProperty DeviceProperty =
+            DependencyProperty.Register(
+                "Device", 
+                typeof(DeviceInfoViewModel), 
+                typeof(SmallDeviceInfoControl), 
+                new PropertyMetadata(null)
+                );
+
+        public SmallDeviceInfoControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
