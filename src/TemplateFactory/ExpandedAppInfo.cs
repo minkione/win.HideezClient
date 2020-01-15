ecf463c6430a1a0b077db044579cb61f8be6fa55
Illya Irachek <illyairachek@gmail.com>
2020-01-15 18:58:05 +0200

    Added app with placeholder styles that allows to list currently running applications and copy their description to clipboard

---
+++ src/TemplateFactory/ExpandedAppInfo.cs
@@ -0,0 +1,62 @@
+﻿using Hideez.ARM;
+using MvvmExtentions;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace TemplateFactory
+{
+    public class ExpandedAppInfo : PropertyChangedImplementation
+    {
+        AppInfo appInfo;
+        Icon icon;
+
+        public AppInfo AppInfo
+        {
+            get
+            {
+                return appInfo;
+            }
+            set
+            {
+                if (appInfo != value)
+                {
+                    appInfo = value;
+                    NotifyPropertyChanged();
+                };
+            }
+        }
+
+        public Icon Icon
+        {
+            get
+            {
+                return icon;
+            }
+            set
+            {
+                if (icon != value)
+                {
+                    Icon = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public ExpandedAppInfo(AppInfo info)
+        {
+            AppInfo = info;
+            try
+            {
+                var proc = Process.GetProcessById(AppInfo.ProcessId);
+
+                if (proc != null)
+                    Icon = ProcessExtensions.GetIcon(proc);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ src/TemplateFactory/MainWindowViewModel.cs
@@ -0,0 +1,81 @@
+﻿using Hideez.ARM;
+using MvvmExtentions;
+using MvvmExtentions.Commands;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TemplateFactory
+{
+    class MainWindowViewModel : PropertyChangedImplementation
+    {
+        bool refreshing = false;
+
+        public MainWindowViewModel() : base()
+        {
+            RefreshAppList();
+        }
+
+        public ObservableCollection<ExpandedAppInfo> Apps { get; } = new ObservableCollection<ExpandedAppInfo>();
+
+        public bool Refreshing
+        {
+            get
+            {
+                return refreshing;
+            }
+            set
+            {
+                if (refreshing != value)
+                {
+                    refreshing = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand RefreshAppListCommand
+        {
+            get
+            {
+                return new DelegateCommand
+                {
+                    CommandAction = (x) =>
+                    {
+                        RefreshAppList();
+                    }
+                };
+            }
+        }
+
+        async void RefreshAppList()
+        {
+            try
+            {
+                Refreshing = true;
+
+                await Task.Run(() =>
+                {
+                    // TODO: AppInfo is returned with empty ProcessId
+                    var apps = AppInfoFactory.GetVisibleAppsInfo();
+                    var uniqueApps = apps.GroupBy(x => x.Description).Select(a => a.First());
+
+                    var expandedApps = uniqueApps.Select(a => new ExpandedAppInfo(a)).ToList();
+
+                    App.Current.Dispatcher.Invoke(() =>
+                    {
+                        Apps.Clear();
+                        foreach (var app in expandedApps)
+                            Apps.Add(app);
+                    });
+                });
+            }
+            finally
+            {
+                Refreshing = false;
+            }
+        }
+    }
+}
