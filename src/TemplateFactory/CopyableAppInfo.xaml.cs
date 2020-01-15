31941f5b00b61565a0643b8034186d7932658700
Illya Irachek <illyairachek@gmail.com>
2020-01-15 18:58:05 +0200

    Added app with placeholder styles that allows to list currently running applications and copy their description to clipboard
    (cherry picked from commit ecf463c6430a1a0b077db044579cb61f8be6fa55)

---
+++ src/TemplateFactory/CopyableAppInfo.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Hideez.ARM;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TemplateFactory
+{
+    /// <summary>
+    /// Interaction logic for CopyableAppInfo.xaml
+    /// </summary>
+    public partial class CopyableAppInfo : UserControl
+    {
+        public AppInfo AppInfo
+        {
+            get { return (AppInfo)GetValue(AppInfoProperty); }
+            set { SetValue(AppInfoProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AppInfoProperty =
+            DependencyProperty.Register(
+                "AppInfo", 
+                typeof(AppInfo), 
+                typeof(CopyableAppInfo), 
+                new PropertyMetadata(new AppInfo())
+                );
+
+        public CopyableAppInfo()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CopyToClipboard();
+        }
+
+        void CopyToClipboard()
+        {
+            string textToCopy = AppInfo.ProcessName.Trim();
+
+            if (!string.IsNullOrWhiteSpace(AppInfo.Description))
+                textToCopy = AppInfo.Description.Trim();
+
+            Clipboard.SetText(textToCopy);
+        }
+    }
+}
