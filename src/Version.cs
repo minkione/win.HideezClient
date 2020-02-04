7dd39374b703c70f3d846f8a8441dc4b508665ae
Illya Irachek <illyairachek@gmail.com>
2020-02-04 10:56:23 +0200

    v3.2.7.1

--- src/Version.cs
+++ src/Version.cs
@@ -13,6 +13,6 @@ using System.Resources;
 // [assembly: AssemblyVersion("1.0.*")]
 
 // Client application version
-[assembly: AssemblyVersion("3.2.7")]
-[assembly: AssemblyFileVersion("3.2.7")]
+[assembly: AssemblyVersion("3.2.7.1")]
+[assembly: AssemblyFileVersion("3.2.7.1")]
 [assembly: NeutralResourcesLanguage("en")]
