617348b7154c215e22c21b887ba6c23d165404ad
Illya Irachek <illyairachek@gmail.com>
2020-04-15 20:18:02 +0300

    Updated cert hash

--- src/scripts/signing_variables.bat
+++ src/scripts/signing_variables.bat
@@ -1,4 +1,4 @@
 @echo off
 set TOOLPATH="C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe"
 set CERTNAME="Hideez Group Inc"
-set CERTSHA1="d16cb2a6282ef06e94c0c35a3f2157f18dec91b4"
+set CERTSHA1="39c9af37a33a57e72f5d72ed864fef60bf84f35d"
