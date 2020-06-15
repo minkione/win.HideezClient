1a35f5d6bdfe03b8a70b81b99ee057f27e8d0bda
Vlad Panasiuk <pv.hideez@gmail.com>
2020-06-15 09:53:36 +0300

    Added a way to determine the type of unlock method

---
+++ src/HideezMiddleware/HideezMiddleware/SessionUnlockMethodMonitor.cs
@@ -0,0 +1,58 @@
+﻿using Hideez.SDK.Communication;
+using Hideez.SDK.Communication.Log;
+using HideezMiddleware.DeviceConnection;
+using HideezMiddleware.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HideezMiddleware
+{
+    public class SessionUnlockMethodMonitor : Logger
+    {
+        readonly ConnectionFlowProcessor _connectionFlowProcessor;
+        readonly TapConnectionProcessor _tapProcessor;
+        readonly RfidConnectionProcessor _rfidProcessor;
+        readonly ProximityConnectionProcessor _proximityProcessor;
+
+        UnlockSessionSwitchProc _unlockProcedure = null;
+        readonly object _upLock = new object();
+
+        internal UnlockSessionSwitchProc UnlockProcedure { get => _unlockProcedure; }
+
+        public SessionUnlockMethodMonitor(ConnectionFlowProcessor connectionFlowProcessor,
+                                          TapConnectionProcessor tapProcessor,
+                                          RfidConnectionProcessor rfidProcessor,
+                                          ProximityConnectionProcessor proximityProcessor,
+                                          ILog log): base(nameof(SessionUnlockMethodMonitor), log)
+        {
+            _connectionFlowProcessor = connectionFlowProcessor;
+            _tapProcessor = tapProcessor;
+            _rfidProcessor = rfidProcessor;
+            _proximityProcessor = proximityProcessor;
+
+            _connectionFlowProcessor.Started += ConnectionFlowProcessor_Started;
+        }
+
+        void ConnectionFlowProcessor_Started(object sender, string e)
+        {
+            lock (_upLock)
+            {
+                if (_unlockProcedure != null)
+                    _unlockProcedure.Dispose();
+
+                _unlockProcedure = new UnlockSessionSwitchProc(e, _connectionFlowProcessor, _tapProcessor, _rfidProcessor, _proximityProcessor);
+                WriteLine("Started unlock procedure");
+            }
+        }
+
+        public SessionSwitchSubject GetUnlockMethod()
+        {
+            if (_unlockProcedure == null)
+                return SessionSwitchSubject.NonHideez;
+            else return _unlockProcedure.UnlockMethod;
+        }
+    }
+}
