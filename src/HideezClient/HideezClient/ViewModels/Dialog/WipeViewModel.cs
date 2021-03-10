3c5ca83401de88fa7d480510c829e4bed3f9c1fe
Illya Irachek <illyairachek@gmail.com>
2021-03-10 19:49:33 +0200

    Updated wipe dialog layout and strings

--- src/HideezClient/HideezClient/ViewModels/Dialog/WipeViewModel.cs
+++ src/HideezClient/HideezClient/ViewModels/Dialog/WipeViewModel.cs
@@ -1,27 +1,38 @@
 ﻿using HideezClient.Messages.Dialogs.Wipe;
+using HideezClient.Modules.DeviceManager;
 using HideezClient.Mvvm;
 using Meta.Lib.Modules.PubSub;
 using MvvmExtensions.Commands;
 using System.Windows.Input;
+using System.Linq;
 
 namespace HideezClient.ViewModels.Dialog
 {
     public class WipeViewModel : ObservableObject
     {
         readonly IMetaPubSub _messenger;
+        readonly IDeviceManager _deviceManager;
 
+        string _vaultName = string.Empty;
         string _deviceId = string.Empty;
         bool _inProgress = false;
 
-        public WipeViewModel(IMetaPubSub messenger)
+        public WipeViewModel(IMetaPubSub messenger, IDeviceManager deviceManager)
         {
             _messenger = messenger;
+            _deviceManager = deviceManager;
         }
 
         public string DeviceId
         {
             get { return _deviceId; }
             set { Set(ref _deviceId, value); }
+        }
+
+        public string VaultName
+        {
+            get { return _vaultName; }
+            set { Set(ref _vaultName, value); }
         }
 
         public bool InProgress
@@ -61,6 +72,7 @@ namespace HideezClient.ViewModels.Dialog
         public void Initialize(string deviceId)
         {
             DeviceId = deviceId;
+            VaultName = _deviceManager.Devices.FirstOrDefault(d => d.Id == DeviceId)?.Name;
         }
 
         void OnCancelWipe()
