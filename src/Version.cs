1aee68d3d7ce7b5e27941f646118d526bc21200e
Illya Irachek <illyairachek@gmail.com>
2021-04-23 13:35:12 +0300

    Release build hotfix

--- src/Version.cs
+++ src/Version.cs
@@ -13,6 +13,6 @@ using System.Resources;
 // [assembly: AssemblyVersion("1.0.*")]
 
 // Client application version
-[assembly: AssemblyVersion("3.8.2")]
-[assembly: AssemblyFileVersion("3.8.2")]
+[assembly: AssemblyVersion("3.8.2.2")]
+[assembly: AssemblyFileVersion("3.8.2.2")]
 [assembly: NeutralResourcesLanguage("en")]
