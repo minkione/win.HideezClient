9f4cb6a7efeacb23561901dd2310a64c8b9daab5
Illya Irachek <illyairachek@gmail.com>
2021-04-23 11:22:09 +0300

    Updated certificate

--- src/scripts/signing_variables.bat
+++ src/scripts/signing_variables.bat
@@ -1,4 +1,4 @@
 @echo off
 set TOOLPATH="C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe"
 set CERTNAME="Hideez Group Inc"
-set CERTSHA1="39c9af37a33a57e72f5d72ed864fef60bf84f35d"
+set CERTSHA1="1202031da128e402d3ec38b5e1fde2c0e2e51b91"
